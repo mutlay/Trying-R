e255e9ad0fe9aa08c718fee00e17d47129dd23fd
mutlay <ibrahim_mutlay@hotmail.com>
2016-04-01 19:46:38 +0300

    End of the project

--- testingR.R
+++ testingR.R
@@ -1932,3 +1932,36 @@ axis(1, at = c(-pi, -pi/2, 0, pi/2, pi),
                        "0", "$\\pi/2$", "$\\pi$"),
                      latex2exp))
 par(op)
+
+
+
+#########################################################################################
+###############                         30                          #####################
+###############      Plotting Time Series Yahoo Finance data        #####################
+#########################################################################################
+
+# http://blog.revolutionanalytics.com/2015/08/plotting-time-series-in-r.html
+library(ggplot2)
+library(xts)
+
+# Get IBM and Linkedin stock data from Yahoo Finance
+ibm_url <- "http://real-chart.finance.yahoo.com/table.csv?s=IBM&a=07&b=24&c=2010&d=07&e=24&f=2015&g=d&ignore=.csv"
+lnkd_url <- "http://real-chart.finance.yahoo.com/table.csv?s=LNKD&a=07&b=24&c=2010&d=07&e=24&f=2015&g=d&ignore=.csv"
+
+yahoo.read <- function(url) {
+  dat <- read.table(url, header = TRUE, sep = ",")
+  df <- dat[,c(1,5)]
+  df$Date <- as.Date(as.character(df$Date))
+  return(df)
+}
+
+ibm <- yahoo.read(ibm_url)
+link <- yahoo.read(lnkd_url)
+
+ggplot(ibm, aes(Date, Close)) +
+  geom_line(aes(color="ibm")) +
+  geom_line(data=link, aes(color="link")) +
+  labs(color="Legend") +
+  scale_colour_manual("", breaks = c("ibm", "link"), values = c("blue", "brown")) +
+  ggtitle("Closing Stock Prices: IBM & LinkedIn") +
+  theme(plot.title = element_text(lineheight = .7, face="bold"))
